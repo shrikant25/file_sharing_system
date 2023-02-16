f2db54599084b6e1796efd4cb1e84759bcab3944
shrikant wankhede <shrikantwankhede24@gmail.com>
2023-02-16 16:53:43 +0530

    updated

--- include/processor_db.h
+++ include/processor_db.h
@@ -16,7 +16,7 @@ int retrive_commr_from_database(char *);
 typedef struct database_info{
     
     PGconn *connection;
-    unsigned int statement_count = 6;
+    unsigned int statement_count = 8;
     
     char *statement_names[statement_count] = {
                                 "s0" // store data in database ;
@@ -25,19 +25,23 @@ typedef struct database_info{
                                 "s3" // store (comms received from senders) into database;
                                 "s4" // retrive commr from database intended for receiver;
                                 "s5" // retrive comms from database intended for sender;
+                                "s6" // update for_sender table set status as 2 i.e "connection establishment in progress";
+                                "s7" // update for_sender table set status as 3 i.e "connection established"
                               };
 
 
     char statments[statement_count] = {
                         "INSERT INTO raw_data (data) values($1)";
-                        "SELECT FD, DATA FROM send_data limit 1";
+                        "SELECT fd, data FROM send_data limit 1";
                         "INSERT INTO open_connections (fd, ipaddr) VALUES($1, $2)";
                         "INSERT INTO  senders_comm (msgid, status) VALUES($1, $2)";
-                        "SELECT FD FROM for_reciever";
-                        "SELECT ipaddr FROM for_sender";
+                        "SELECT cid, fd FROM for_reciever where status = 1 limit 1";
+                        "SELECT ipaddr FROM for_sender where status = 1 limit 1";
+                        "UPDATE for_sender set status = 2 where cid = ($1)";
+                        "UPDATE for_sender set status = 3 where cid = ($!)";
                         }; 
 
-    int param_count[statement_count] = { 1, 0, 2, 2, 0, 0};
+    int param_count[statement_count] = { 1, 0, 2, 2, 0, 0, 1, 1};
 
 }database_info;
 
--- processor_db.c
+++ processor_db.c
@@ -41,7 +41,6 @@ int store_data_in_database(char *data) {
 
     PGresult *res = NULL;
     const char param_values[dbinf.param_count[0]] = {data};
-    
 
     res = PQexecPrepared(dbinf.connection, dbinf.statement_names[0], 
                                     param_count[0], param_values, NULL, NULL, 0);
@@ -72,10 +71,13 @@ int retrive_data_from_database(char *data) {
     if (row_count > 0) {
         memcpy(data, PQgetvalue(res, 0, 0), PQgetlength(res, 0, 0));
         memcpy(data+2, PQgetvalue(res, 0, 1), PQgetlength(res, 0, 1));
+        PQclear(res);
         return 0;
     }
 
+    PQclear(res);
     return 1;
+
     
 }
 
@@ -88,7 +90,7 @@ int store_commr_into_database(char *data) {
     char *ptr = data;
     PGresult *res = NULL;
 
-    if (*ptr == 1){
+    if (*ptr == 1) {
 
         ptr++;
         memcpy(fd, *ptr, 2);   
@@ -118,8 +120,9 @@ int store_comms_into_database(char *data) {
     char msg_id[16];
     char msg_status[1]; 
     char *ptr = data;
+    PGresult* res = NULL;
 
-    if (*ptr == 1){
+    if (*ptr == 1) {
 
         ptr++;
         memcpy(msg_id, *ptr, 16);   
@@ -129,7 +132,7 @@ int store_comms_into_database(char *data) {
 
         const char param_values[dbinf.param_count[3]] = {msg_id, msg_status};
         
-        PGresult* res = PQexecPrepared(dbinf.connection, dbinf.statement_names[3], dbinf.param_count[3], 
+        res = PQexecPrepared(dbinf.connection, dbinf.statement_names[3], dbinf.param_count[3], 
                                         param_values, NULL, NULL, 0);
 
     }
@@ -161,19 +164,22 @@ int retrive_commr_from_database(char *data) {
     row_count = PQntuples(res);
     if (row_count > 0) {
         memcpy(data, PQgetvalue(res, 0, 0), PQgetlength(res, 0, 0));
+        PQclear(res);
         return 0;
     }
 
+    PQclear(res);
     return 1;
 }
 
 
 int retrive_comms_from_database(char *data) {
 
+    int cid = 0;
     int row_count = 0;
     PGresult *res = NULL;
 
-    res = PQexecPrepared(dbinf.connection, dbinf.statement_names[5], param_count[5], param_values, NULL, NULL, 0);
+    res = PQexecPrepared(dbinf.connection, dbinf.statement_names[5], param_count[5], NULL, NULL, NULL, 0);
 
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
         printf("Insert failed: %s\n", PQerrorMessage(dbinf.connection));
@@ -182,10 +188,29 @@ int retrive_comms_from_database(char *data) {
 
     row_count = PQntuples(res);
     if (row_count > 0) {
+    
         memcpy(data, PQgetvalue(res, 0, 0), PQgetlength(res, 0, 0));
+        PQclear(res);
+    
+        // update this row from database
+        cid = *(int *)data;
+        const char *param_values[dbinf.param_count[6]] = {cid};        
+        res = PQexecPrepared(dbinf.connection, dbinf.statement_names[6], param_count[6], param_values, NULL, NULL, 0);
+
+        if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+            printf("Insert failed: %s\n", PQerrorMessage(dbinf.connection));
+            PQclear(res);
+   
+            return 1;
+        }   
+
+        PQclear(res);
+
         return 0;
+    
     }
 
+    PQclear(res);
     return 1;
 
 }
