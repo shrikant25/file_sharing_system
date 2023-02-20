8186f3acce69c4948bc42ec9a82d3475e622b7fd
shrikant wankhede <shrikantwankhede24@gmail.com>
2023-02-21 04:10:59 +0530

    updated

---
+++ daemon.c
@@ -0,0 +1,72 @@
+
+#include <libpq-fe.h>
+#include <libpq/libpq-fs.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <syslog.h>
+
+
+static void skeleton_daemon(){
+ 
+    pid_t pid;
+
+    // make port of parent process
+    pid = fork();
+
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    // kill parent
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    // if >= 0 menas chidl process has beocmme session leader
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    //fork again
+    pid = fork();
+
+    
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    // again parent dies
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    // set file premissions
+    umask(0);
+
+}
+
+int main(){
+    skeleton_daemon();
+
+    while (1){
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "First daemon started.");
+
+        
+
+        sleep (20);
+        break;
+    }
+
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
+
+//gcc daemon.c -o daemon  -I/usr/include/postgresql -lpq -std=c99
