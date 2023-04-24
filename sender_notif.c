53e1ee77376acfe49b7e18756b6b67d45468c3af
shrikant wankhede <shrikantwankhede24@gmail.com>
2023-04-24 08:37:30 +0530

    added code for sender_notif.c

--- sender_notif.c
+++ sender_notif.c
@@ -1,69 +1,92 @@
 #include <stdio.h>
+#include <semaphore.h>
+#include <string.h>
+#include <syslog.h>
+#include <fcntl.h>
 #include <libpq-fe.h>
+#include "processor_r.h"
+#include "shared_memory.h"
+
+PGconn *connection;
+
+void store_log(char *logtext) 
+{
+
+    PGresult *res = NULL;
+    char log[100];
+    strncpy(log, logtext, strlen(logtext));
+
+    const char *const param_values[] = {log};
+    const int paramLengths[] = {sizeof(log)};
+    const int paramFormats[] = {0};
+    int resultFormat = 0;
+    
+    res = PQexecPrepared(connection, "store_log", 5, param_values, paramLengths, paramFormats, 0);
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        syslog(LOG_NOTICE, "logging failed %s , log %s\n", PQerrorMessage(connection), log);
+    }
+
+    PQclear(res);
+}
+
 
 int main(void) {
 
-    PGconn *connection;
     PGresult *res;
+    PGnotify *notify;
+    sem_t *sigps;
+    char error[100];
+
     connection = PQconnectdb("user = shrikant dbname = shrikant ");
     if (PQstatus(connection) != CONNECTION_OK) {
         syslog(LOG_NOTICE, "failed to create connection to database %s", PQerrorMessage(connection));
         return -1;
     }
 
-
-    PGresult *res = PQexec(conn, "LISTEN mychannel");
+    res = PQprepare(connection, "store_logs", "INSERT INTO LOGS (log) VALUES ($1)", 1, NULL);
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        fprintf(stderr, "LISTEN command failed: %s", PQerrorMessage(conn));
+        syslog(LOG_NOTICE, "Preparation of statement failed: %s\n", PQerrorMessage(connection));
         PQclear(res);
-        PQfinish(conn);
-        exit(1);
+        PQfinish(connection);
+        return -1;
     }
+    PQclear(res);
 
-    while (1) {
-    if (PQconsumeInput(conn) == 0) {
-        fprintf(stderr, "Failed to consume input: %s", PQerrorMessage(conn));
-        PQfinish(conn);
-        exit(1);
+    res = PQexec(connection, "LISTEN mychannel");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        memset(error, 0, sizeof(error));
+        sprintf(error, "LISTEN command failed: %s", PQerrorMessage(connection));
+        store_log(error);
+        PQclear(res);
+        PQfinish(connection);
+        return -1;
     }
-    PGnotify *notify;
-    while ((notify = PQnotifies(conn)) != NULL) {
-        printf("Received notification from channel %s: %s\n", notify->relname, notify->extra);
-        PQfreemem(notify);
-    }
-}
+    PQclear(res);
 
-
-
-
-}
-
-connection = PQconnectdb("user = shrikant dbname = shrikant");
-
-    if (PQstatus(connection) != CONNECTION_OK) {
-        syslog(LOG_NOTICE, "Connection to database failed: %s\n", PQerrorMessage(connection));
+    if ((sigps = sem_open(SEM_LOCK_SIG_PS, O_CREAT, 0777, 0)) == SEM_FAILED) {
+        memset(error, 0, sizeof(error));
+        sprintf(error, "error creating semphore sigps: %s", PQerrorMessage(connection));
+        store_log(error);
         return -1;
     }
 
-    return 0;
-}
+    while (1) {
 
-
-int prepare_statements() 
-{    
-    int i, status = 0;
-
-    for (i = 0; i<statement_count; i++){
-
-        PGresult* res = PQprepare(connection, dbs[i].statement_name, dbs[i].statement, dbs[i].param_count, NULL);
-        if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-            syslog(LOG_NOTICE, "Preparation of statement failed: %s\n", PQerrorMessage(connection));
-            status = -1;
-            PQclear(res);
-            break;
+        if (PQconsumeInput(connection) == 0) {
+            memset(error, 0, sizeof(error));
+            sprintf(error, "Failed to consume input: %s", PQerrorMessage(connection));
+            store_log(error);
         }
 
-        PQclear(res);
+        while ((notify = PQnotifies(connection)) != NULL) {
+            sem_post(sigps);
+            PQfreemem(notify);
+        }
+    
     }
     
-    return status;
+    sem_close(sigps);
+    PQfinish(connection);
+
+    return 0;
+}
