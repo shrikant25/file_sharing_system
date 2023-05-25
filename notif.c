35c9e30a44d8c4bc1df466360dd918ed6305993f
shrikant wankhede <shrikantwankhede24@gmail.com>
2023-05-25 13:42:53 +0530

    updated code for notif.c

--- notif.c
+++ notif.c
@@ -5,12 +5,17 @@
 #include <syslog.h>
 #include <fcntl.h>
 #include <libpq-fe.h>
+#include <sys/epoll.h>
 #include "shared_memory.h"
 
+int dbsocket;
+int epoll_fd;
+char error[100];
+char noti_channel[30];
+struct epoll_event event;
 PGconn *connection;
 semlocks sem_lock_sig;
 
-char error[100];
 
 void store_log(char *logtext) 
 {
@@ -32,6 +37,7 @@ void store_log(char *logtext)
     PQclear(res);
 }
 
+
 int initnotif(char *confg_filename) 
 {
     
@@ -39,7 +45,6 @@ int initnotif(char *confg_filename)
     char buf[500];
     char username[30];
     char dbname[30];
-    char noti_channel[30];
     char noti_command[100];
     char db_conn_command[100];
 
@@ -49,6 +54,7 @@ int initnotif(char *confg_filename)
         return -1;
     }
 
+    memset(error, 0, sizeof(error));
     memset(buf, 0, sizeof(buf));
     memset(noti_channel, 0 , sizeof(noti_channel));
     memset(db_conn_command, 0, sizeof(db_conn_command));
@@ -79,60 +85,108 @@ int initnotif(char *confg_filename)
         PQfinish(connection);
         return -1;
     }
+
+    dbsocket = PQsocket(connection);
+    if (dbsocket == -1) {
+        sprintf(error, "noti channel %s, error in PQsocket creating a connection to db %s", noti_channel, PQerrorMessage(connection));
+        store_log(error);
+        PQfinish(connection);
+        return -1;
+    }
+    PQsetnonblocking(connection, 1);
+
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        sprintf(error, "epoll_create1 failed in notifier listening on %s", noti_channel);
+        store_log(error);
+        PQfinish(connection);
+        return -1;
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = dbsocket;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, dbsocket, &event) == -1) {
+        sprintf(error, "epollctl failed in notifier listening on %s", noti_channel);
+        store_log(error);
+        PQfinish(connection);
+        close(epoll_fd);
+        return -1;
+    }
+
     PQclear(res);
-
-
     res = PQexec(connection, noti_command);
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        memset(error, 0, sizeof(error));
-        sprintf(error, "LISTEN command failed: %s", PQerrorMessage(connection));
+        sprintf(error, "noti channel %s, LISTEN command failed: %s", noti_channel, PQerrorMessage(connection));
         store_log(error);
         PQclear(res);
+        close(epoll_fd);
         PQfinish(connection);
         return -1;
     }
     PQclear(res);
     
     if ((sem_lock_sig.var = sem_open(sem_lock_sig.key, O_CREAT, 0777, 0)) == SEM_FAILED) {
-        memset(error, 0, sizeof(error));
-        sprintf(error, "error creating semphore %s: %s", sem_lock_sig.key, PQerrorMessage(connection));
+        sprintf(error, "noti channel %s error creating semphore %s: %s", noti_channel, sem_lock_sig.key, PQerrorMessage(connection));
         store_log(error);
+        close(epoll_fd);
+        PQfinish(connection);
         return -1;
     }
+
     return 0;
 }
 
 
 int main(int argc, char *argv[]) 
 {
-
     PGresult *res;
     PGnotify *notify;
+    int num_events;
 
     if (argc != 2) {
         syslog(LOG_NOTICE, "invlaid arguments");
     }
     
     if(initnotif(argv[1]) == -1) {return -1;}
-
+    
     while (1) {
 
-        if (PQconsumeInput(connection) == 0) {
+        num_events = epoll_wait(epoll_fd, &event, 1, -1);
+        if (num_events == -1) {
             memset(error, 0, sizeof(error));
-            sprintf(error, "Failed to consume input: %s", PQerrorMessage(connection));
+            sprintf(error, "epoll wait failed for notifier listening on channel %s", noti_channel);
             store_log(error);
+            break;
         }
-        else{
-            while ((notify = PQnotifies(connection)) != NULL) {
-                sem_post(sem_lock_sig.var);
-                PQfreemem(notify);
+
+        if (event.data.fd == dbsocket) {
+        
+            if (PQconsumeInput(connection) == 0) {
+                memset(error, 0, sizeof(error));
+                sprintf(error, "noti channel %s Failed to consume input: %s", noti_channel, PQerrorMessage(connection));
+                store_log(error);
+                break;
+            }
+            else {
+                while ((notify = PQnotifies(connection)) != NULL) {
+                    sem_post(sem_lock_sig.var);
+                    PQfreemem(notify);
+                }
             }
         }
-    
+        else {
+            memset(error, 0, sizeof(error));
+            sprintf(error, "unkown event occured on epoll of notifier listening on channel %s", noti_channel);
+            store_log(error);
+            break;
+        }    
     }
     
+    store_log("closing notif");
     sem_close(sem_lock_sig.var);
     PQfinish(connection);
+    close(epoll_fd);
 
     return 0;
 }
